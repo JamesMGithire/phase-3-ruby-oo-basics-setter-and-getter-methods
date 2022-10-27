6238206978e2be5dcc5196db5f9d89b1ef5b023f
James Mburu Githire <jamesmburug24@gmail.com>
2022-10-27 09:20:48 +0300

    major : fixed Person class>Rspec-passing

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,16 @@
 class Person
-  attr_accessor :name
-  attr_accessor :job
-  
+  # attr_accessor :name
+  # attr_accessor :job
+  def name
+    @name
+  end
+  def name=(value)
+    @name = value
+  end
+  def job
+    @job
+  end
+  def job=(value)
+    @job = value
+  end
 end
